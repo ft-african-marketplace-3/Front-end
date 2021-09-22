306c4b25701ff0a8b70a8aaac1dec478fbe177f6
Nelson Maldonado <play2nino@gmail.com>
2021-09-21 17:01:03 -0700

    linked contact and working on Contact page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from "react"
 import styled from "styled-components"
-import { Route, Switch } from 'react-router-dom';
-
-import NavBar from "./Components/NavBar";
-import Home from "./Components/Home";
+import { Route, Switch } from "react-router-dom"
+import Contact from "./Components/Contact"
+import NavBar from "./Components/NavBar"
+import Home from "./Components/Home"
 // import SignUp from "./Components/SignUp"
 // import Login from "./Components/Login"
 // import About from "./Components/About"
@@ -11,20 +11,21 @@ import Home from "./Components/Home";
 export default function App() {
   return (
     <StyledApp>
-      <NavBar/>
+      <NavBar />
       <Switch>
         {/* <Route path={"/about"} component={}/>
         <Route path={"/signup"} component={}/>
         <Route path={"/login"} component={}/> */}
-        <Route path={"/"} component={Home}/>
+        <Route path={"/contact"} component={Contact} />
+        <Route path={"/"} component={Home} />
       </Switch>
     </StyledApp>
-  );
+  )
 }
 
 const StyledApp = styled.div`
-width: 100%;
-height: auto;
-display: flex ;
-flex-direction: column;
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
 `
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,7 +1,28 @@
 import React from "react"
+import "../Contact.css"
 
 function Contact() {
-  return <div>Contact Page</div>
+  return (
+    <div className="contactMainDiv">
+      <h1>Contact Page</h1>
+      <div className="leftSideDiv">
+        <div className="contactInfo">
+          <p>
+            Get in touch with us for more information about Sauti and our
+            services. We are happy to answer any questions you may have!
+          </p>
+          <div className="contactDetails">
+            <p className="contactTitle">Contact Details</p>
+            <p>
+              {" "}
+              The Foundry, Viking House, Waiyaki way, Westlands, Nairobi, Kenya
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="formSideDiv">Form goes here</div>
+    </div>
+  )
 }
 
 export default Contact
