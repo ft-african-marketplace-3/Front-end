44452d48eee4f1674c5148c767594b5649460f8d
jonahjtr <86381032+jonahjtr@users.noreply.github.com>
2021-09-22 13:11:20 -0500

    fixed login auth bug

--- src/Components/LoginPage/LoginContainter.js
+++ src/Components/LoginPage/LoginContainter.js
@@ -1,123 +1,116 @@
-import React, {useState, useEffect} from "react";
-import { useHistory } from 'react-router-dom';
-import LogInForm from './Login';
-import styled from 'styled-components'
-import schema from '../validations/loginSchema';
-import axios from 'axios';
-import * as yup from 'yup';
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import LogInForm from "./Login";
+import styled from "styled-components";
+import schema from "../validations/loginSchema";
+import axios from "axios";
+import * as yup from "yup";
 
 const initialFormValues = {
-    username: '',
-    password: '',
-}
+  username: "",
+  password: "",
+};
 const initialFormErrors = {
-    username: '',
-    password: '',
-}
+  username: "",
+  password: "",
+};
 
 // const initialLogIn = []
-const initialDisabled = true
+const initialDisabled = true;
 
 export default function LogIn() {
   // const [logIn, setLogIn] = useState(initialLogIn)
-  const [formValues, setFormValues] = useState(initialFormValues)
-  const [formErrors, setFormErrors] = useState(initialFormErrors)
-  const [disabled, setDisabled] = useState(initialDisabled)
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
 
   const { push } = useHistory();
 
   const updateForm = (inputName, inputValue) => {
-    setFormValues({ ...formValues, [inputName]: inputValue});
-  }
-
-
-  const postNewLogIn = e => {
-    e.preventDefault();
-    axios
-      .post('https://fieldmarketbackend.herokuapp.com/api/users', formValues)
-      .then(resp => {
-        console.log(resp);
-        localStorage.setItem("token", resp.data.token);
-        localStorage.setItem("role", resp.data.role);
-        localStorage.setItem("username", resp.data.username);
-        push('/listing');
-      })
-      .catch(err => {
-        debugger
-        console.log(err);
-      })
-  }
+    setFormValues({ ...formValues, [inputName]: inputValue });
+  };
 
   const validate = (name, value) => {
     yup
       .reach(schema, name)
       .validate(value)
       .then((valid) => {
-        setFormErrors(
-          {...formErrors,[name]: '',}
-        );
+        setFormErrors({ ...formErrors, [name]: "" });
       })
       .catch((err) => {
-        setFormErrors(
-          { ...formErrors,[name]: err.errors[0]}
-        );
+        setFormErrors({ ...formErrors, [name]: err.errors[0] });
       });
-  }
+  };
 
   const inputChange = (name, value) => {
     validate(name, value);
     setFormValues({ ...formValues, [name]: value });
   };
 
-  const formSubmit = () => {
-      const newUserLoggingIn = {
-        username: formValues.username.trim(),
-        password: formValues.password.trim(),
-      }
-      postNewLogIn(newUserLoggingIn);
-  }
+  const formSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("https://web44scaffolding.herokuapp.com/api/user/login", formValues)
+      .then((resp) => {
+        console.log(resp);
+        localStorage.setItem("token", resp.data.token);
+        localStorage.setItem("role", resp.data.role);
+        localStorage.setItem("username", resp.data.username);
+        push("/listing");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   useEffect(() => {
     schema.isValid(formValues).then((valid) => {
-      setDisabled(!valid)
-     });
-  }, [formValues])
+      setDisabled(!valid);
+    });
+  }, [formValues]);
 
-  return(
-      <StyledLogin className='log-in-div'>
-          <LogInForm className='form-container'
-            values={formValues}
-            update={updateForm}
-            change={inputChange}
-            submit={formSubmit}
-            disabled={disabled}
-            errors={formErrors}
-          />
-      </StyledLogin>
-  )
+  return (
+    <StyledLogin className="log-in-div">
+      <LogInForm
+        className="form-container"
+        values={formValues}
+        update={updateForm}
+        change={inputChange}
+        submit={formSubmit}
+        disabled={disabled}
+        errors={formErrors}
+      />
+    </StyledLogin>
+  );
 }
 
-const StyledLogin = styled.div `
-font-family: 'Roboto Condensed', sans-serif;
-width:100%;
-height:93vh;
-display: flex;
-justify-content: center;
-background: linear-gradient(155deg, rgb(29, 26, 26), rgb(29, 26, 26), darkgray);
-  background-size: 400% 400%;
-	animation: gradient 12s ease infinite;
-  @keyframes gradient {
-	0% {
-		background-position: 0% 50%;
-	}
-	50% {
-		background-position: 100% 50%;
-	}
-	100% {
-		background-position: 0% 50%;
-  }}
-form{
-  text-align:center;
+const StyledLogin = styled.div`
+  font-family: "Roboto Condensed", sans-serif;
+  width: 100%;
+  height: 93vh;
   display: flex;
+  justify-content: center;
+  background: linear-gradient(
+    155deg,
+    rgb(29, 26, 26),
+    rgb(29, 26, 26),
+    darkgray
+  );
+  background-size: 400% 400%;
+  animation: gradient 12s ease infinite;
+  @keyframes gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+  form {
+    text-align: center;
+    display: flex;
     flex-direction: column;
     align-items: center;
     margin: 5% 0;
@@ -127,64 +120,62 @@ form{
     border-radius: 40px;
     background-color: rgba(255, 255, 255, 0.3);
     padding: 1%;
-    
-}
-.info{
-  height: 20vh;
-  display: flex;
-  flex-direction: column;
-}
-h2{
-  font-family: 'Alegreya Sans SC', sans-serif;
-  margin-top: 10%;
-}
-.username-login{
-  text-align: left;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-top: 10%;
-
-}
-.username-login input {
-  height: 2rem;
-  width: 50%;
-  text-align: left;
-  margin-left: 1%;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      background-color: #ddd;
-      padding: 2px 8px;
-      border: 2px solid #444;
-      border-radius: 1rem;
-      font-size: 1rem;
-      padding: 0 2rem;
-}
-.password-login{
-  text-align: left;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 10%;
-}
-.password-login input{
-  height: 2rem;
-  width: 50%;
-  margin-left: 1%;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      background-color: #ddd;
-      padding: 2px 8px;
-      border: 2px solid #444;
-      border-radius: 1rem;
-      font-size: 1rem;
-      padding: 0 2rem;
-    }
-#login-button{
+  }
+  .info {
+    height: 20vh;
+    display: flex;
+    flex-direction: column;
+  }
+  h2 {
+    font-family: "Alegreya Sans SC", sans-serif;
+    margin-top: 10%;
+  }
+  .username-login {
+    text-align: left;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 10%;
+  }
+  .username-login input {
+    height: 2rem;
+    width: 50%;
+    text-align: left;
+    margin-left: 1%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: #ddd;
+    padding: 2px 8px;
+    border: 2px solid #444;
+    border-radius: 1rem;
+    font-size: 1rem;
+    padding: 0 2rem;
+  }
+  .password-login {
+    text-align: left;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 10%;
+  }
+  .password-login input {
+    height: 2rem;
+    width: 50%;
+    margin-left: 1%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: #ddd;
+    padding: 2px 8px;
+    border: 2px solid #444;
+    border-radius: 1rem;
+    font-size: 1rem;
+    padding: 0 2rem;
+  }
+  #login-button {
     width: 60%;
     height: 3rem;
     border-radius: 2.5rem;
@@ -197,21 +188,20 @@ h2{
     background-color: #008b57;
   }
   #login-button:hover {
-    
     color: #ffffff;
     filter: brightness(120%);
   }
-  a{
+  a {
     text-decoration: none;
     color: #edd97f;
   }
-  a:hover{
+  a:hover {
     text-decoration: underline;
   }
-  .new-user{
+  .new-user {
     height: 10vh;
     display: flex;
     justify-content: center;
     align-items: center;
   }
-`
+`;
