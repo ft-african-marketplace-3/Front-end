5611178e326d1cc64139d56320bc37581304d580
Ogden-R <rebeccaogden97@gmail.com>
2021-09-24 13:20:21 -0500

    added button to itempage, but i dont remember the name of the url extension to get there.

--- src/Components/newItems/ItemPage.js
+++ src/Components/newItems/ItemPage.js
@@ -3,6 +3,7 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import styled from 'styled-components'
+import { Link } from 'react-router-dom';
 
 import ItemCard from './ItemCard';
 
@@ -24,6 +25,7 @@ const ItemPage = () => {
     // Returns
     return (
         <StyledItems className = 'ItemPage'>
+            <Link to="/new-listing">Add New Listing</Link>
             <div className={"dataTable"}>
             <ItemCard className = 'Items' items = {items} dark={true}/>
             </div>
@@ -44,6 +46,30 @@ flex-direction: column;
 align-items: center;
 justify-content: center;
 align-content: center;
+
+a {
+    color: rgba(29, 26, 26, 1);
+    margin: 2%;
+    font-size: 1.5rem;
+    font-family: "Roboto Condensed", sans-serif;
+    position: absolute;
+    top: 60%;
+    left: 49%;
+    transform: translate(-50%, -50%);
+    z-index: 2;
+    font-weight: 900;
+    letter-spacing: 0.04em;
+    white-space: nowrap;
+    text-decoration: none;
+    background: rgba(245, 245, 245, 0.8);
+    padding: 0 1%;
+    transition: ease 0.5s;
+  }
+  a:hover {
+    filter: brightness(150%);
+    background: rgba(245, 245, 245, 1);
+    transform: translate(-50%, -50%) scale(1.05);
+    transition: ease 0.2s;
 
 .dataTable{
     background: center;
