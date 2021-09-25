fc25c9ac88107bf0cf7a683cad5cbf56ce69d156
BrendonHawkins <artbitcodedata@gmail.com>
2021-09-24 20:24:04 -0400

    fixed errors preventing deploy

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { StyledEngineProvider } from '@mui/material/styles';
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import reducer from "./Components/reducers/Data";
+// import { Provider } from "react-redux";
+// import { createStore } from "redux";
+// import reducer from "./Components/reducers/Data";
 // import isLoggedInReducer from "./Components/Reducers/User";
 import { BrowserRouter as Router } from "react-router-dom";
 
@@ -16,16 +16,16 @@ import { BrowserRouter as Router } from "react-router-dom";
 //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 // );
 
-const store = createStore(reducer)
+// const store = createStore(reducer)
 
 
 ReactDOM.render(
   <StyledEngineProvider injectFirst>  
-    <Provider store={store}>
+    {/* <Provider store={store}> */}
       <Router>
         <App />
       </Router>
-    </Provider>
+    {/* </Provider> */}
   </StyledEngineProvider>  ,
   document.getElementById('root')
 );
