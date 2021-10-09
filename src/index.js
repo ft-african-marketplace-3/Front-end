41ddae55e26c0bcb996153b83ca2c4f40632a5a0
BrendonHawkins <artbitcodedata@gmail.com>
2021-10-09 18:34:41 -0400

    reducers store

--- src/index.js
+++ src/index.js
@@ -4,28 +4,27 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { StyledEngineProvider } from '@mui/material/styles';
-// import { Provider } from "react-redux";
-// import { createStore } from "redux";
-// import reducer from "./Components/reducers/Data";
-// import isLoggedInReducer from "./Components/Reducers/User";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "./Components/reducers/Index";
 import { BrowserRouter as Router } from "react-router-dom";
 
 
-// const store = createStore(
-//   isLoggedInReducer,
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-// );
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 // const store = createStore(reducer)
 
 
 ReactDOM.render(
   <StyledEngineProvider injectFirst>  
-    {/* <Provider store={store}> */}
+    <Provider store={store}>
       <Router>
         <App />
       </Router>
-    {/* </Provider> */}
+    </Provider>
   </StyledEngineProvider>  ,
   document.getElementById('root')
 );
